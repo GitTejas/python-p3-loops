704d3619203fce3f934d8351d85fd36c4e4d200d
Tejas Nampalli <TSNampalli@gmail.com>
2024-04-22 17:12:08 -0400

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i=10
+    while i > 0:
+        print(f'{i}')
+        i-=1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    squared_nums = []
+    for num in int_list:
+        squared_num = num*num
+        squared_nums.append(squared_num)
+    return squared_nums
     pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(f"{i}")
+        i+=1
